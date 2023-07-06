f302fed5e47957c5940b26a6dc6840228d1697a2
Onkgopotse007 <72313429+Onkgopotse007@users.noreply.github.com>
2023-07-06 20:56:02 +0200

    characters commit before services architecture

---
+++ Models/Characters.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPG_dotnet.Models
+{
+    public class Characters
+    {
+        public int id { get; set; } = 10;
+        public string name { get; set; } = "Shirou Emiya";
+        public int hitpoints { get; set; } = 10;
+        public int strength { get; set; } = 10;
+        public int defense { get; set; } = 10;
+        public int intelligence { get; set; } = 10;
+        public RpgClass fighterClass {get; set;} = RpgClass.Archer;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+global using RPG_dotnet.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
