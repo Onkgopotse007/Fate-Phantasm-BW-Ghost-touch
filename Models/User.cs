5759810049797d873e493c95dbf064a28775d281
Onkgopotse007 <rt19001209@studentmail.biust.ac.bw>
2025-06-03 14:37:28 +0200

    User character completely removed in favour of a loadout system,  honestly what was I thinking

--- Models/User.cs
+++ Models/User.cs
@@ -17,26 +17,5 @@ namespace RPG_dotnet.Models
         public byte[] passwordHash { get; set; } = new byte[0];
         public byte[] passwordSalt { get; set; } = new byte[0];
         public int userRole {get; set;} = 2;
-        public ICollection<UserCharacter> userCharacters { get; set; }
-
-        [NotMapped]
-        public ICollection<Characters> characters
-        {
-            get => userCharacters?.Select(uc => uc.character).ToList();
-            set
-            {
-                if (value.Count > 7)
-                {
-                    throw new InvalidOperationException("A user cannot have more than 7 characters.");
-                }
-
-                if (value.Select(c => c.fighterClass).Distinct().Count() != value.Count)
-                {
-                    throw new InvalidOperationException("A user cannot have repeating character classes.");
-                }
-
-                userCharacters = value.Select(c => new UserCharacter { user = this, character = c }).ToList();
-            }
-        }
     }
 }
