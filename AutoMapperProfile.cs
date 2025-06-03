942b0b01fd4b275af227f037a9d9579071799237
Onkgopotse007 <rt19001209@studentmail.biust.ac.bw>
2025-06-03 15:15:15 +0200

    Automapper change for loadout

--- AutoMapperProfile.cs
+++ AutoMapperProfile.cs
@@ -9,19 +9,37 @@ namespace RPG_dotnet
     {
         public AutoMapperProfile()
         {
+            // Character mappings
             CreateMap<Characters, GetCharacterDto>();
             CreateMap<AddCharacterDto, Characters>();
             CreateMap<UpdateCharacterDto, Characters>();
-            CreateMap<UserCharacter, GetCharacterDto>()
+
+            // Ability mappings
+            CreateMap<Ability, GetAbilityDto>();
+            CreateMap<AddAbilityDto, Ability>();
+
+            // Loadout mappings
+            CreateMap<CreateLoadoutDto, Loadout>();
+            CreateMap<LoadoutCharacter, GetCharacterDto>()
                 .ForMember(dest => dest.id, opt => opt.MapFrom(src => src.character.id))
                 .ForMember(dest => dest.name, opt => opt.MapFrom(src => src.character.name))
                 .ForMember(dest => dest.hitpoints, opt => opt.MapFrom(src => src.character.hitpoints))
                 .ForMember(dest => dest.strength, opt => opt.MapFrom(src => src.character.strength))
                 .ForMember(dest => dest.defense, opt => opt.MapFrom(src => src.character.defense))
                 .ForMember(dest => dest.intelligence, opt => opt.MapFrom(src => src.character.intelligence))
-                .ForMember(dest => dest.fighterClass, opt => opt.MapFrom(src => src.character.fighterClass));
-            CreateMap<Ability, GetAbilityDto>();
-            CreateMap<AddAbilityDto, Ability>();
+                .ForMember(dest => dest.mana, opt => opt.MapFrom(src => src.character.mana))
+                .ForMember(dest => dest.movement, opt => opt.MapFrom(src => src.character.movement))
+                .ForMember(dest => dest.baseDamage, opt => opt.MapFrom(src => src.character.baseDamage))
+                .ForMember(dest => dest.manaGainPerAttack, opt => opt.MapFrom(src => src.character.manaGainPerAttack))
+                .ForMember(dest => dest.role, opt => opt.MapFrom(src => src.character.role))
+                .ForMember(dest => dest.fighterClass, opt => opt.MapFrom(src => src.character.fighterClass))
+                .ForMember(dest => dest.abilities, opt => opt.MapFrom(src => src.character.abilities));
+            CreateMap<Loadout, GetLoadoutDto>()
+                .ForMember(dest => dest.loadoutId, opt => opt.MapFrom(src => src.loadoutId))
+                .ForMember(dest => dest.name, opt => opt.MapFrom(src => src.name))
+                .ForMember(dest => dest.characters, opt => opt.MapFrom(src =>
+                    src.characters.Select(lc => lc.character) // key fix here
+                ));
         }
     }
 }
